9088ec2dcb4ec5d08841378f4a77fc0d9ad4abd8
David Atkins <atki1080@gmail.com>
2019-11-21 13:21:25 -0700

    Add instance variable testers

--- test/event_test.rb
+++ test/event_test.rb
@@ -8,4 +8,17 @@ class EventTest < Minitest::Test
     event = Event.new("Curling", [24, 30, 18, 20, 41])
     assert_instance_of Event, event
   end
+
+  def test_name_is_correct
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal "Curling", event.name
+  end
+
+  def test_event_is_correct
+    event = Event.new("Curling", [24, 30, 18, 20, 41])
+    expected = [24, 30, 18, 20, 41]
+    assert_equal expected, event.ages
+  end
+
+
 end
