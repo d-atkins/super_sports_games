b1a0c22e53119bf6d37915ec983fb0e52637e1c5
David Atkins <atki1080@gmail.com>
2019-11-21 14:33:50 -0700

    Update runner

--- super_sports_games_runner.rb
+++ super_sports_games_runner.rb
@@ -1,6 +1,20 @@
 require './lib/games'
 
 print "Enter the year for the games: "
-year = gets.chomp
-games = Games.new(year.to_i)
-require "pry"; binding.pry
+input = gets.chomp
+games = Games.new(input.to_i)
+
+while (input != "q")
+  puts "------------------------------------------"
+  puts ">[A]dd event      >[S]ummary      >[Q]uit"
+  input = gets.chomp.downcase
+  if input == "a"
+    print "Enter name of event: "
+    event = gets.chomp
+    # while (input != "q")
+    #   print "Enter participant age ([Q] to quit): "
+    # end
+  elsif input == "s"
+    puts games.summary
+  end
+end
